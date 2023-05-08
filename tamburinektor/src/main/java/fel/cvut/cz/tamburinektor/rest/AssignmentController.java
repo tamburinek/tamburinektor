543c52067dddea264a232e86f9a5e6c49a0ceb74
norbaart <norbaart@fel.cvut.cz>
2023-05-08 17:03:34 +0200

    update test question

--- tamburinektor/src/main/java/fel/cvut/cz/tamburinektor/rest/AssignmentController.java
+++ tamburinektor/src/main/java/fel/cvut/cz/tamburinektor/rest/AssignmentController.java
@@ -19,6 +19,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -59,6 +61,21 @@ public class AssignmentController {
     }
 
     @PreAuthorize("hasRole('ROLE_TEACHER')")
+    @PatchMapping(value = "/assignment", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updateAssignment(@RequestBody AssignmentDto dto){
+        Assignment assignment = assignmentService.getById(dto.getId());
+        assignment.setQuestion(dto.getQuestion());
+        assignment.setImageLink(dto.getImageLink());
+        assignment.setRightAnswers(dto.getRightAnswers());
+        assignment.setWrongAnswers(dto.getWrongAnswers());
+        assignment.setOpenQuestion(dto.isOpenQuestion());
+        assignmentService.createOrUploadAssignment(assignment);
+        log.info("Assignment updated {}", assignment.getQuestion());
+        final HttpHeaders headers = RestUtil.createLocationHeaderNewUri("/assignment/{id}", assignment.getId());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PreAuthorize("hasRole('ROLE_TEACHER')")
     @GetMapping(value = "/assignment/close", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<AssignmentDto> getAllAssignmentByMeClose(){
         User user = userService.getCurrentUser();
@@ -72,5 +89,11 @@ public class AssignmentController {
         User user = userService.getCurrentUser();
         List<Assignment> assignments = assignmentService.getAllByUserOpen(user);
         return assignments.stream().map(mapper::toDto).collect(Collectors.toList());
+    }
+
+    @GetMapping(value = "/assignment/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public AssignmentDto getAssignmentById(@PathVariable Long id){
+        Assignment assignment = assignmentService.getById(id);
+        return mapper.toDto(assignment);
     }
 }
